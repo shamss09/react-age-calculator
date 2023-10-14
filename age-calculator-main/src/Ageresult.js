9f6c557e237ab63d0086cda4674f37fbfe3639da
shamss09 <144318609+shamss09@users.noreply.github.com>
2023-10-14 22:05:30 +0530

    Delete age-calculator-main directory

--- age-calculator-main/src/Ageresult.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Ageresult = ({age}) => {
-  return (
-    <h4>You are {age.years} years, {age.months} months, and {age.days} days old.</h4>
-  );
-}
-
-export default Ageresult;
